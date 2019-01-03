592246c0779a2099639eb19311b94a544eed0b2e
rmesterca <ralucamesterca@gmail.com>
2019-01-03 10:18:39 +0100

    implemented tab switch animation

--- TabSwitcher/TabSwitcher/TabSwitcher/TabSwitcherViewController.swift
+++ TabSwitcher/TabSwitcher/TabSwitcher/TabSwitcherViewController.swift
@@ -20,7 +20,7 @@ class TabSwitcherViewController: UIViewController {
     //MARK: Properties
     private var behindTabAlpha: CGFloat = 0.5
     private var frontTabAlpha: CGFloat = 1
-    private lazy var animator = UIViewPropertyAnimator(duration: 5, curve: .easeInOut)
+    private var animateDuration: TimeInterval = 2
 
     //MARK: LifeCycle
     override func viewDidLoad() {
@@ -50,66 +50,67 @@ extension TabSwitcherViewController {
         rightTabView.isUserInteractionEnabled.toggle()
     }
 
-    fileprivate func scissorTabAnimation(tabViews: [TabView]) { //, onCompletion: (() -> ())?) {
+    fileprivate func scissorTabAnimation(tabViews: [TabView]) {
 
-//        animator.isUserInteractionEnabled = false
+        var angles: [CGFloat?] = []
+        tabViews.forEach { (view) in
+            let angle = view.isLeftTabView ? CGFloat.pi/8 : CGFloat.pi/(-8)
+            angles.append(angle)
+        }
 
-            UIView.animateKeyframes(withDuration: self.animator.duration, delay: 0, animations: {
-
-                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
-
-                    tabViews.forEach({ (view) in
-                        let angle = view.isLeftTabView ? CGFloat.pi/(-4) : CGFloat.pi/4
-                        view.transform = CGAffineTransform(rotationAngle: angle)
-                    })
-
-//                    self.switchTabsPosition(frontTab: tabViews[0], behindTab: tabViews[1])
-                    self.switchTabsAlpha(frontTab: tabViews[0], behindTab: tabViews[1])
-                })
-
-                UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.2, animations: {
-                    self.switchTabsPosition(frontTab: tabViews[0], behindTab: tabViews[1])
-//                    tabViews[0].layer.zPosition = 100
-//                    tabViews[1].layer.zPosition = 50
-//                    self.view.layoutSubviews()
-
-
-
-
-
-                })
-
-                UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.2, animations: {
-                    tabViews[0].alpha = self.frontTabAlpha
-                    tabViews[1].alpha = self.behindTabAlpha
-//                    self.switchTabsAlpha(frontTab: tabViews[0], behindTab: tabViews[1])
-
-                })
-
-                UIView.addKeyframe(withRelativeStartTime: 0.7, relativeDuration: 0.3, animations: {
-                    tabViews.forEach({ (view) in
-                        view.transform = .identity
-                    })
-                })
-            })
+        getAnimationForTab(view: tabViews[0], angle:  angles[0] ?? 0, zPosition: [50, 100], xPosition: 200, alpha: [behindTabAlpha, frontTabAlpha])
+        getAnimationForTab(view: tabViews[1], angle: angles[1] ?? 0, zPosition: [100, 50], xPosition: -200, alpha: [frontTabAlpha, behindTabAlpha])
     }
 
+    func createGroupAnimation(angle: CGFloat, zPosition: [CGFloat], xPosition: CGFloat, alpha: [CGFloat]) -> CAAnimationGroup {
+        let translateAnimation = createTranslateAnimation(xPosition: xPosition)
+        let transformAnimation = createRotationAnimation(angle: angle)
+        let positionAnimation = createPositionAnimation(zPosition: zPosition)
+        let opacityAnimation = createOpacityAnimation(alpha: alpha)
 
-    fileprivate func switchTabsPosition(frontTab: UIView, behindTab: UIView) {
-//        self.view.bringSubviewToFront(frontTab)
-//
+        let groupAnimation = CAAnimationGroup()
+        groupAnimation.animations = [translateAnimation, transformAnimation, positionAnimation, opacityAnimation]
+        groupAnimation.duration = animateDuration
 
-//        self.view.layoutSubviews()
-
-        UIView.transition(with: self.view, duration: 10, options: .transitionCrossDissolve, animations: {
-            frontTab.layer.zPosition = 100
-            behindTab.layer.zPosition = 50
-        }, completion: nil)
+        return groupAnimation
     }
 
-    fileprivate func switchTabsAlpha(frontTab: UIView, behindTab: UIView) {
-        frontTab.alpha = frontTabAlpha
-        behindTab.alpha = behindTabAlpha
+    func createTranslateAnimation(xPosition: CGFloat) -> CAKeyframeAnimation {
+        let translateAnimation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        translateAnimation.values = [0, xPosition, 0]
+        translateAnimation.keyTimes = [0, 0.2, 1]
+        return translateAnimation
+    }
+
+    func createPositionAnimation(zPosition: [CGFloat]) -> CAKeyframeAnimation {
+        let positionAnimation = CAKeyframeAnimation(keyPath: "transform.translation.z")
+        positionAnimation.values = zPosition
+        positionAnimation.duration = animateDuration
+        positionAnimation.keyTimes = [0, 0.5, 1]
+        return positionAnimation
+    }
+
+    func createRotationAnimation(angle: CGFloat) -> CAKeyframeAnimation {
+        let transformAnimation = CAKeyframeAnimation(keyPath: "transform.rotation")
+        transformAnimation.values = [0, angle, 0]
+        transformAnimation.keyTimes = [0.2, 0.5, 1]
+
+        return transformAnimation
+    }
+
+    func createOpacityAnimation(alpha: [CGFloat]) -> CAKeyframeAnimation {
+        let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        opacityAnimation.values = alpha
+        opacityAnimation.keyTimes = [0, 0.5]
+        return opacityAnimation
+
+    }
+
+    func getAnimationForTab(view: TabView, angle: CGFloat, zPosition: [CGFloat], xPosition: CGFloat, alpha: [CGFloat]) {
+        let animation = createGroupAnimation(angle: angle, zPosition: zPosition, xPosition: xPosition, alpha: alpha)
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        view.layer.add(animation, forKey: "viewAnimation")
     }
 }
 
